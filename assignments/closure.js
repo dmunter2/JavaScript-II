107b0c6be1e9621b1fa0498f0430c0fb43d573d8
dmunter2 <devon.munter@gmail.com>
2019-05-26 21:17:37 -0600

    changes

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,38 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function drivecar(drivingAge){
+  var a = ' years until you can drive!';
+  return function(yearOfBirth){
+    var age = 2019 - yearOfBirth;
+
+    if (age >= drivingAge){
+      console.log('nice u can drive');
+    } else{
+      console.log((16 - age) + ' years until you can drive!')
+    }
+  }
+}
+
+drivecar(16)(1996);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+const counter = (function(x) {
+  return function(){
+    x += 1;
+    return x;
+  }
+}(0));
+console.log(counter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
